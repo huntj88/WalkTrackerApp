b2203458e4bd719cbb74ca82de1931b8650fce15
huntj88 <huntj88@gmail.com>
2020-04-21 23:31:24 -0500

    Bottom nav functional

--- app/src/main/java/me/jameshunt/walkhistory/MainActivity.kt
+++ app/src/main/java/me/jameshunt/walkhistory/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
 import kotlinx.android.synthetic.main.activity_main.*
 import me.jameshunt.walkhistory.map.MapWrapperFragment
 import me.jameshunt.walkhistory.track.PermissionManager
@@ -30,6 +31,25 @@ class MainActivity : AppCompatActivity() {
             override fun getItemCount(): Int {
                 return 2
             }
+        }
+
+        viewPager.registerOnPageChangeCallback(object: ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                bottomNavigationView.selectedItemId = when(position) {
+                    0 -> R.id.track_walk
+                    1 -> R.id.map_walks
+                    else -> throw NotImplementedError()
+                }
+            }
+        })
+
+        bottomNavigationView.setOnNavigationItemSelectedListener { menuItem ->
+            when(menuItem.itemId) {
+                R.id.track_walk -> viewPager.setCurrentItem(0, true)
+                R.id.map_walks -> viewPager.setCurrentItem(1, true)
+                else -> throw NotImplementedError()
+            }
+            true
         }
     }
 
